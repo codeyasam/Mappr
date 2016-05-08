2c5c566a20d9275fe8f9aeb0e358676da2c51df3
Emmanuel Yasa <codeyasam@gmail.com>
2016-05-09 04:31:57 +0800

    [php - admin navigation] comments link to management of PLAN DURATION

--- includes/admin_nav.php
+++ includes/admin_nav.php
@@ -1,7 +1,7 @@
 <nav>
 	<ul>
 		<li><a href="index.php">Admin Main Panel</a></li>
-		<li><a href="managePlanduration.php">Manage Plan Duration</a></li>
+<!-- 		<li><a href="managePlanduration.php">Manage Plan Duration</a></li> -->
 		<li><a href="managePlan.php">Manage Plans</a></li>
 		<li><a href="manageCategory.php">Manage Establishment Category</a></li>
 		<li><a href="../logout.php">logout</a></li>
