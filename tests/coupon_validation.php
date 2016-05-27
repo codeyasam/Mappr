3b4922f5a77db12b83caeeb55ed2c51a5fd50b82
Emmanuel Yasa <codeyasam@gmail.com>
2016-05-27 22:00:38 +0800

    [PHP - tests] adds a working test for both amount off and percent off coupon

--- tests/coupon_validation.php
+++ tests/coupon_validation.php
@@ -3,12 +3,21 @@
 	$answer = true;
 
 	try {
-		$coupon = \Stripe\Coupon::retrieve("456");
+		$coupon = \Stripe\Coupon::retrieve("asdf");
 	} catch (\Stripe\Error\InvalidRequest $e) {
 		$answer = false;
 	}
 
 	if ($answer === true) {
-		var_dump($coupon->valid);
+		//var_dump($coupon->valid);
+		echo "<pre>";
+			print_r($coupon);
+		echo "<pre>";
+		//echo $coupon->percent_off;	
+		if ($coupon->valid && !empty($coupon->percent_off)) {
+			echo $coupon->percent_off . " %";
+		} else {
+			echo $coupon->amount_off / 100;
+		}
 	}
 ?>
