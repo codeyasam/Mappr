7e546fa20fc9b4763bc4d30b72c52997b74c4eb4
Emmanuel Yasa <codeyasam@gmail.com>
2016-05-30 14:42:53 +0800

    [PHP, STRIPE - Invoices] implements stripes invoice to paymentSummary to be view by users as references to their transaction

--- Public/paymentSummary.php
+++ Public/paymentSummary.php
@@ -5,7 +5,12 @@
 	$condition['key'] = "owner_id";
 	$condition['value'] = $user->id;
 	$condition['isNumeric'] = true;
-	$transactions = SubsPlan::find_all($condition);
+	//$transactions = SubsPlan::find_all($condition);
+	$transactions = \Stripe\Invoice::all(array("customer" => $user->stripe_id));
+	// echo "<pre>";
+	// 	print_r($transactions);
+	// echo "</pre>";
+	// var_dump($transactions["data"][0]);
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,11 +19,36 @@
 	</head>
 	<body>
 	<?php include("../includes/navigation.php"); ?>
-	<?php foreach($transactions as $key => $each_transac):  ?>
-		<?php $plan = Plan::find_by_id($each_transac->plan_id); ?>
-		<div>
-			<p><?php echo $plan->toString(); ?><a href="">CHECK DETAILS</a></p>
-		</div>
-	<?php endforeach; ?>
+	<table border="1">
+		<tr>
+			<th>PRICE</th>
+			<th>DISCOUNT</th>
+			<th>AMOUNT PAID</th>
+			<th>DATE: (timezone - server)</th>
+		</tr>	
+	<?php for ($i = 0; $i < count($transactions['data']); $i++) { ?>
+		<?php $each_transac = $transactions['data'][$i]; ?>
+		<?php $total_amount = $each_transac->total / 100; ?>
+		<?php $subtotal = $each_transac->subtotal / 100; ?>
+		<?php  
+			$discount = "none";
+			if (!empty($each_transac->discount)) {
+				$coupon = $each_transac->discount->coupon;
+				if (!empty($coupon->percent_off)) {
+					$discount = $coupon->percent_off . "%";
+				} else {
+					$discount = $coupon->amount_off / 100 . " " . $coupon->currency;
+				}
+			}
+  		?>
+		<tr>
+			<td><?php echo htmlentities($subtotal); ?></td>
+			<td><?php echo htmlentities($discount); ?></td>
+			<td><?php echo htmlentities($total_amount); ?></td>
+			<td><?php echo htmlentities(format_date(get_mysql_datetime($each_transac->date))); ?></td>
+		</tr>
+	<?php } ?>		
+	</table>
+
 	</body>
 </html>
