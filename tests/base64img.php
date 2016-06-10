8a68d0fa9eec7a3669c45321f1e0ebbe8ced4577
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-11 03:09:36 +0800

    [PHP - ANDROID] adds register feature, posts request with images encoded with base64 string

---
+++ tests/base64img.php
@@ -0,0 +1,33 @@
+<?php require_once("../includes/initialize.php"); ?>
+<?php  
+	// if (isset($_POST['image'])) {
+	// 	echo '{"success":"' . $_POST['image'] . '"}';
+	// 	$decodedImage = base64_decode($_POST['image']);
+	// 	file_put_contents("../testImages/testImg.jpg", $decodedImage);
+	// }
+	// else echo '{"success":"false"}';
+
+
+	if (isset($_POST['submit'])) {
+		$user = new User();
+		$user->first_name = isset($_POST['first_name']) ? trim($_POST['first_name']) : "";
+		$user->last_name = isset($_POST['last_name']) ? trim($_POST['last_name']) : "";		
+		$user->username = isset($_POST['username']) ? trim($_POST['username']) : "";		
+		$user->password = isset($_POST['password']) ? md5(trim($_POST['password'])) : "";
+		$user->email = isset($_POST['email']) ? trim($_POST['email']) : "";		
+
+		if ($user->create()) {
+			if (isset($_POST['image'])) {
+				$decodedImage = base64_decode($_POST['image']);
+				file_put_contents("../Public/DISPLAY_PICTURES/profile_pic".$user->id, $decodedImage);
+				$user->display_picture = "DISPLAY_PICTURES/profile_pic".$user->id;
+			} else {
+				$user->display_picture = $_POST['display_picture'];
+			}	
+			$user->update();
+			echo '{"success":"true"}';		
+		}
+	} else {
+		echo '{"success":"false"}';
+	}
+?>
