0b1ef286d1eeea25f7c1e96c09a871fd79919f8b
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-13 03:26:03 +0800

    [PHP, ANDROID] adds hasReview and branchreview json in getting the current user

--- tests/getCurrentUser.php
+++ tests/getCurrentUser.php
@@ -2,7 +2,15 @@
 <?php  
 	if (isset($_GET['user_id'])) {
 		$user = User::find_by_id($_GET['user_id']);
-		echo "{" . $user->toJSON() . "}";	
+		$result_array = BranchReview::find_by_sql("SELECT * FROM REVIEW_TB WHERE user_id = " . $user->id);
+		$branch_review = !empty($result_array) ? array_shift($result_array) : false;
+		$outputString = $user->toJSON();
+		if ($branch_review) {
+			$outputString .= ', "hasReview":"true", ' . $branch_review->toJSON();
+		} else {
+			$outputString .= ', "hasReview":"false"';
+		}
+		echo "{" . $outputString . "}";	
 	}
 	
 ?>
