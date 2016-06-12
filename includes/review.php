7560b47897f59c0bc74150b0672028951a1ca780
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-12 19:27:50 +0800

    [PHP, MYSQL] adds submit date field to both review tb and model

--- includes/review.php
+++ includes/review.php
@@ -4,11 +4,12 @@
 	class EstabReview extends DatabaseObject {
 
 		protected static $table_name = "REVIEW_TB";
-		protected static $db_fields = array('user_id', 'estab_id', 'rating', 'comment');
+		protected static $db_fields = array('user_id', 'estab_id', 'rating', 'comment', 'submit_date');
 
 		public $user_id;
 		public $estab_id;
 		public $rating;
 		public $comment;
+		public $submit_date;
 	}
 ?>
