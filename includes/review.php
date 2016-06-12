dd1861e56cf9b5822db2868762a79c0f3f2e45a5
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-13 03:24:48 +0800

    [PHP, MYSQL] adds id field in branchReview table and model to support update functionality

--- includes/review.php
+++ includes/review.php
@@ -1,11 +1,12 @@
 <?php require_once(LIB_PATH . DS . "database.php"); ?>
 <?php require_once(LIB_PATH . DS . "databaseObject.php"); ?>
 <?php  
-	class EstabReview extends DatabaseObject {
+	class BranchReview extends DatabaseObject {
 
 		protected static $table_name = "REVIEW_TB";
-		protected static $db_fields = array('user_id', 'branch_id', 'rating', 'comment', 'submit_date');
+		protected static $db_fields = array('id', 'user_id', 'branch_id', 'rating', 'comment', 'submit_date');
 
+		public $id;
 		public $user_id;
 		public $branch_id;
 		public $rating;
