584297bcfba131e3d9902558d19a1ae7db107b26
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-24 02:48:38 +0800

    [PHP - Review Model] adds a getBranchReview function that takes branch id as an argument and returns jsonString of reviews, users, average rating

--- includes/review.php
+++ includes/review.php
@@ -1,5 +1,6 @@
 <?php require_once(LIB_PATH . DS . "database.php"); ?>
 <?php require_once(LIB_PATH . DS . "databaseObject.php"); ?>
+<?php require_once(LIB_PATH . DS . "functions.php"); ?>
 <?php  
 	class BranchReview extends DatabaseObject {
 
@@ -12,5 +13,33 @@
 		public $rating;
 		public $comment;
 		public $submit_date;
+
+		public static function getBranchReview($branch_id) {
+			global $database;
+
+			$userArrObj = array();
+			$reviewArrObj = array();
+
+			$branch_id = $database->escape_value($branch_id);
+			$sql = "SELECT e.id as 'user_id', e.first_name, e.last_name, e.display_picture, r.id as 'review_id', r.branch_id, r.rating, r.comment, r.submit_date FROM REVIEW_TB r, END_USER_TB e, BRANCHES_TB b WHERE b.id = " . $branch_id . " AND r.user_id = e.id AND r.branch_id = b.id";
+			$result_set = $database->query($sql);
+			if (empty($result_set)) return false;
+			
+			$reviews_count = $database->num_rows($result_set);
+			$average_rating = 0;
+
+			while($row = $database->fetch_array($result_set)) {
+				$userArrObj[] = User::instantiate($row, $row['user_id']);
+				$reviewArrObj[] = BranchReview::instantiate($row, $row['review_id']);
+				$average_rating += $row['rating'];
+			}
+
+			$average_rating = (float)$average_rating / $reviews_count;
+
+			$jsonString  = createJSONEntity("Reviews", $reviewArrObj) . ", ";
+			$jsonString .= createJSONEntity("Users", $userArrObj) . ", ";
+			$jsonString .= '"average_rating":' . '"' . $average_rating . '", ';
+			return $jsonString; 
+		}
 	}
 ?>
