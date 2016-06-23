bb5a530f4de8dc1c486dcee9585167b2aec9664e
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-24 02:49:50 +0800

    [PHP, ANDROID - getFullDetails] implements getBranchReview

--- tests/getFullDetails.php
+++ tests/getFullDetails.php
@@ -14,6 +14,8 @@
 	$branch = EstabBranch::find_by_id($branch_id);
 	$estab = Establishment::find_by_id($branch->estab_id);
 	$gallery = EstabGallery::find_by_sql("SELECT g.gallery_pic FROM GALLERY_TB g, BRANCHES_GALLERY_TB b WHERE g.id = b.gallery_id AND b.branch_id = " . $branch_id);
+	$reviews = BranchReview::getBranchReview($branch_id);
+	if ($reviews) $jsonString .= $reviews;
 	$jsonString .= '"branch":{' . $branch->toJSON() . '}, "estab":{' . $estab->toJSON() . '},';
 	$jsonString .= !empty($gallery) ? createJSONEntity("Gallery", $gallery) : '"Gallery":[]';
 	echo '{' . $jsonString . '}';
