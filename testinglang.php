d733d27719565fb59d70f6475b2671c51d7d2ffe
Emmanuel Yasa <codeyasam@gmail.com>
2016-07-01 21:35:04 +0800

    [TEST] email and username existence

--- testinglang.php
+++ testinglang.php
@@ -1,28 +1,35 @@
 <?php  
-	require_once("includes/initialize.php");
-	//anonymous function php
-	// $get_addresses = function($obj) {
-	// 	return $obj->address;
-	// };
+    require_once("includes/initialize.php");
+	// //anonymous function php
+	// // $get_addresses = function($obj) {
+	// // 	return $obj->address;
+	// // };
 	
-	// // $arrObj = EstabBranch::find_all();
-	// // $myArr = array_map(function($obj) {return $obj->address;}, $arrObj);
-	// // print_r($myArr);
-	// // echo join("<>", $myArr);
+	// // // $arrObj = EstabBranch::find_all();
+	// // // $myArr = array_map(function($obj) {return $obj->address;}, $arrObj);
+	// // // print_r($myArr);
+	// // // echo join("<>", $myArr);
 
-	// function testlang() {
-	// 	$arrObj = EstabBranch::find_all();
-	// 	$myArr = array_map(function($obj) {return $obj->address;}, $arrObj);
-	// 	print_r($myArr);
-	// 	echo join("<>", $myArr);		
-	// }
+	// // function testlang() {
+	// // 	$arrObj = EstabBranch::find_all();
+	// // 	$myArr = array_map(function($obj) {return $obj->address;}, $arrObj);
+	// // 	print_r($myArr);
+	// // 	echo join("<>", $myArr);		
+	// // }
 
-	// testlang();
+	// // testlang();
 
-	//echo SubsPlanEstab::get_total_branch_plotted(3);
-	$objArr = EstabBranch::find_all();
-	// echo "<pre>";
-	// 	print_r(createJSONEntity("Branches", $objArr));
-	// echo "</pre>";
-	echo "{" . createJSONEntity("Branches", $objArr) . "}";
+	// //echo SubsPlanEstab::get_total_branch_plotted(3);
+	// $objArr = EstabBranch::find_all();
+	// // echo "<pre>";
+	// // 	print_r(createJSONEntity("Branches", $objArr));
+	// // echo "</pre>";
+	// echo "{" . createJSONEntity("Branches", $objArr) . "}";
+	// $subject = "<h2>yeah</h2>";
+	// $result = preg_replace('%([+\"\'<>/]+)%', '\\\\$1', $subject);
+	// echo $result;
+    //var_dump(User::check_username_format("asfasチトシハ"));
+    var_dump(User::check_existing("codeyasam", "username", "has existing username"));
+    var_dump(User::check_existing("codeyasam", "email", "has existing username")); 
+    var_dump($errors);   
 ?>
