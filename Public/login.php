61e307837f3d4aff151524e6162eba441ada0a71
Emmanuel Yasa <codeyasam@gmail.com>
2016-07-01 22:10:28 +0800

    [PHP - login] adds prompts to user

--- Public/login.php
+++ Public/login.php
@@ -1,6 +1,7 @@
 <?php require_once("../includes/initialize.php"); ?>
 <?php $session->is_logged_in() ? redirect_to("index.php") : null; ?>
 <?php 
+	$prompt_to_user = "";
 	if (isset($_POST['submit'])) {
 		$emUsername = $_POST['emUsername'];
 		$password = $_POST['password'];
@@ -10,6 +11,7 @@
 			$session->login($user);
 			User::page_redirect($user->id);
 		}
+		$prompt_to_user = "wrong username or password";
 	}
 ?>
 <!DOCTYPE html>
@@ -22,6 +24,7 @@
 		<form action="login.php" method="POST">
 			<p><input type="text" name="emUsername" value="" placeholder="Email/Username"/></p>
 			<p><input type="password" name="password" value="" placeholder="Password"/></p>
+			<p><?php echo $prompt_to_user; ?></p>
 			<p><input type="submit" name="submit" value="LOGIN"/></p>
 		</form>
 	</body>
