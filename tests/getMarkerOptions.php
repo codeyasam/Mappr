e8df9fd6162ce2a431fe2f2553eeebd0167fd57b
Emmanuel Yasa <codeyasam@gmail.com>
2016-07-03 03:53:39 +0800

    [PHP, ANDROID - getMarkerOptions] adds a condition to the query for displaying ONLY ACTIVE subscriptions

--- tests/getMarkerOptions.php
+++ tests/getMarkerOptions.php
@@ -16,19 +16,21 @@
 			// $estabObjArr = Establishment::find_all(array('key' => 'id', 'value' => $branch->estab_id, 'isNumeric' => true));
 			$sql  = "SELECT b.id as 'branch_id', b.estab_id as 'estab_id', b.address, b.lat, b.lng, ";
 			$sql .= "e.category_id, e.name, e.display_picture ";
-			$sql .= "FROM BRANCHES_TB b, ESTABLISHMENT_TB e ";
+			$sql .= "FROM BRANCHES_TB b, ESTABLISHMENT_TB e, SUBSCRIBED_PLAN sp, SUBSPLAN_ESTAB_TB se ";
 			$sql .= "WHERE b.estab_id = " . $branch->estab_id . " ";
 			$sql .= "AND b.estab_id = e.id ";
+			$sql .= "AND sp.status = 'ACTIVE' AND sp.id = se.subs_plan_id AND b.estab_id = se.estab_id ";
 
 		} else if ($_GET[MAPPR_OPT] == OPT_BY_CATEGORY) {
 			$category_id = $database->escape_value($_GET[CATEGORY_ID]);
 			
 			$sql  = "SELECT b.id as 'branch_id', b.estab_id as 'estab_id', b.address, b.lat, b.lng, ";
 			$sql .= "e.category_id, e.name, e.display_picture ";
-			$sql .= "FROM BRANCHES_TB b, ESTABLISHMENT_TB e, CATEGORY_TB c ";
+			$sql .= "FROM BRANCHES_TB b, ESTABLISHMENT_TB e, CATEGORY_TB c, SUBSCRIBED_PLAN sp, SUBSPLAN_ESTAB_TB se ";
 			$sql .= "WHERE c.id = e.category_id ";
 			$sql .= "AND e.id = b.estab_id ";
-			$sql .= "AND c.id = " . $category_id;
+			$sql .= "AND c.id = " . $category_id . " ";
+			$sql .= "AND sp.status = 'ACTIVE' AND sp.id = se.subs_plan_id AND b.estab_id = se.estab_id ";
 			// $objArr = EstabBranch::find_by_sql($sql);
 			// $estabObjArr = Establishment::find_all(array('key' => 'category_id', 'value' => $category_id, 'isNumeric' => true));
 
@@ -37,9 +39,10 @@
 			
 			$sql  = "SELECT b.id as 'branch_id', b.estab_id as 'estab_id', b.address, b.lat, b.lng, ";
 			$sql .= "e.category_id, e.name, e.display_picture ";
-			$sql .= "FROM BRANCHES_TB b, ESTABLISHMENT_TB e ";
+			$sql .= "FROM BRANCHES_TB b, ESTABLISHMENT_TB e, SUBSCRIBED_PLAN sp, SUBSPLAN_ESTAB_TB se ";
 			$sql .= "WHERE e.name LIKE " . "'%{$search_string}%' ";
 			$sql .= "AND b.estab_id = e.id";
+			$sql .= "AND sp.status = 'ACTIVE' AND sp.id = se.subs_plan_id AND b.estab_id = se.estab_id ";
 		}
 
 		$mapprPlotter = new MapprPlotRetriever();
