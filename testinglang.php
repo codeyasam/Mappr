724e000a938ed2243b96631673955eef856b9489
Emmanuel Yasa <codeyasam@gmail.com>
2016-08-17 19:33:25 +0800

    [PHP - jquery time entry] adds jquery time entry

--- testinglang.php
+++ testinglang.php
@@ -29,7 +29,135 @@
 	// $result = preg_replace('%([+\"\'<>/]+)%', '\\\\$1', $subject);
 	// echo $result;
     //var_dump(User::check_username_format("asfasチトシハ"));
-    var_dump(User::check_existing("codeyasam", "username", "has existing username"));
-    var_dump(User::check_existing("codeyasam", "email", "has existing username")); 
-    var_dump($errors);   
+    // var_dump(User::check_existing("codeyasam", "username", "has existing username"));
+    // var_dump(User::check_existing("codeyasam", "email", "has existing username")); 
+    // var_dump($errors);   
+
+
+    // $branchHours = new BranchHours();
+    // $branchHours->branch_id = 1;
+    // $branchHours->day_no = 3;
+    // $branchHours->opening_hour = time_txt_to_24hour("1:50:00", "am");
+    // $branchHours->closing_hour = time_txt_to_24hour("8:00:00", "pm");
+    // $branchHours->create();
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="Public/js/jquery_timeentry/jquery.timeentry.css"/>
+	</head>
+	<body>
+		<script type="text/javascript" src="Public/js/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="Public/js/jquery-ui.min.js"></script>
+		<script type="text/javascript" src="Public/js/jquery_timeentry/jquery.plugin.js"></script> 
+		<script type="text/javascript" src="Public/js/jquery_timeentry/jquery.timeentry.js"></script>		
+		<script type="text/javascript" src="Public/js/functions.js"></script>
+		<script type="text/javascript">
+
+			function myTimeEntry(inputID, defaultTime) {
+				$(inputID).timeEntry();
+				$(inputID).val(defaultTime);
+			}
+
+			function time_txt_to_24hour(str_time) {
+				period = str_time.substring(5, 7);
+				str_time = str_time.substring(0, 5);
+				str_time = str_time.split(":");
+				
+				if (period == "pm" || period == "PM") {
+					str_time[0] = parseInt(str_time[0]) + 12;
+				}
+
+				return str_time.join(":");
+			}
+
+			var branch_id = 1;
+			var branchHourArray = [];
+			
+			var days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+			var branchHourInputs = "";
+			for (var i = 0; i < days.length; i++) {
+				branchHourInputs += '<div style="width: 700px;"><div style="width: 10%; float: left;" >' + days[i] + ': </div><span>';
+				branchHourInputs += '<input type="radio" class="setTimeManual" name="timeSelection' + i + '" checked="checked" data-internalid="' + i + '" />';
+
+				branchHourInputs += '<input type="text" size="10" id="defaultEntryOpen' + i + '" class="defaultEntryOpen" data-internalid='
+				branchHourInputs += '"' + i + '"' + '/></span>';
+				
+				branchHourInputs += ' to <span>';
+
+				branchHourInputs += '<input type="text" size="10" id="defaultEntryClose' + i + '" class="defaultEntryClose" data-internalid=';
+				branchHourInputs += '"' + i + '"' + '/></span>';
+
+				branchHourInputs += '<input type="radio" class="setTimeClosed" name="timeSelection' + i + '" value="closed" data-internalid="' + i + '"/> closed';
+
+				branchHourInputs += '<input type="radio" class="setTime24Hours" name="timeSelection' + i + '" data-internalid="' + i + '" /> 24 Hours';
+				branchHourInputs += '</div>';
+
+				var branchHour = {branch_id : branch_id, day_no : '', opening_hour : '', closing_hour : ''};
+
+				branchHour.day_no = i;
+				branchHour.opening_hour = time_txt_to_24hour("08:00AM");
+				branchHour.closing_hour = time_txt_to_24hour("08:00PM");
+				branchHourArray[i] = branchHour;
+			}
+
+			$('body').append(branchHourInputs);
+			myTimeEntry(".defaultEntryOpen", "08:00AM");
+			myTimeEntry(".defaultEntryClose", "08:00PM");
+
+			$(document).on('change', '.defaultEntryOpen', function() {
+				var internalID = $(this).attr('data-internalid');
+				console.log(internalID);
+
+				branchHourArray[internalID].opening_hour = time_txt_to_24hour($(this).val());
+				console.log(branchHourArray[internalID]);
+			});
+
+			$(document).on('change', '.defaultEntryClose', function() {
+				var internalID = $(this).attr('data-internalid');
+				branchHourArray[internalID].closing_hour = time_txt_to_24hour($(this).val());
+			});
+			
+			$(document).on('click', '.setTimeManual', function() {
+				var internalID = $(this).attr('data-internalid');
+
+				$('#defaultEntryOpen' + internalID).prop('disabled', false);
+				$('#defaultEntryClose' + internalID).prop('disabled', false);
+
+				var opening_hour = $('#defaultEntryOpen' + internalID).val();
+				var closing_hour = $('#defaultEntryClose' + internalID).val();
+				branchHourArray[internalID].opening_hour = time_txt_to_24hour(opening_hour);
+				branchHourArray[internalID].closing_hour = time_txt_to_24hour(closing_hour);
+			});
+
+			$(document).on('click', '.setTimeClosed', function() {
+				var internalID = $(this).attr('data-internalid');
+				console.log(internalID);
+
+				$('#defaultEntryOpen' + internalID).prop('disabled', true);
+				$('#defaultEntryClose' + internalID).prop('disabled', true);
+				branchHourArray[internalID].opening_hour = "00:00:00";
+				branchHourArray[internalID].closing_hour = "00:00:00";
+			});
+
+			$(document).on('click', '.setTime24Hours', function() {
+				var internalID = $(this).attr('data-internalid');
+				console.log(internalID);
+
+				$('#defaultEntryOpen' + internalID).val('12:00AM');
+				$('#defaultEntryClose' + internalID).val('12:00AM');				
+				$('#defaultEntryOpen' + internalID).prop('disabled', true);
+				$('#defaultEntryClose' + internalID).prop('disabled', true);
+
+				branchHourArray[internalID].opening_hour = time_txt_to_24hour("12:00AM");
+				branchHourArray[internalID].closing_hour = time_txt_to_24hour("12:00PM");
+				
+			});		
+
+			console.log(branchHourArray);
+		</script>
+	</body>
+</html>
