90297ceca341b3bdfb38e4b77f5fdc2d069c3a9e
Emmanuel Yasa <codeyasam@gmail.com>
2016-10-06 07:42:50 +0800

    [PHP - managePlan.php] Fixes the flow of plan management

--- Public/Admin/managePlan.php
+++ Public/Admin/managePlan.php
@@ -13,6 +13,7 @@
 	<head>
 		<title></title>
 		<?php include '../../includes/styles_admin.php'; ?>
+		<link rel="stylesheet" type="text/css" href="../js/jquery-ui.css">
 	</head>
 	<body>
 		<header>
@@ -60,9 +61,8 @@
 			<script type="text/javascript" src="../js/jquery-ui.min.js"></script>
 			<script type="text/javascript" src="../js/functions.js"></script>			
 			<script type="text/javascript">
-
 				processRequest("backendprocess.php?getPlans=true");
-
+				
 				$('#customPlan').hide();
 				$('#planDuration').change(function() {
 					$('#customPlan').hide();
@@ -77,6 +77,17 @@
 					if (objReq.readyState == 4 && objReq.status == 200) {
 						console.log(objReq.responseText);	
 						var jsonObj = JSON.parse(objReq.responseText);
+						
+						if (jsonObj.createdPlan) {
+							$('#estab_no').val('');
+							$('#branch_no').val('');
+							$('#cost').val('');
+							$('#intervalCount').val('');
+							$('#plan_name').val('');		
+							custom_alert_dialog("Successfully created a plan");
+							console.log("created a plan");
+						}
+
 						if (jsonObj.Plans) {
 							var tblRows = "<tr>";
 							tblRows += "<th>ID</th><th>NAME</th><th>INTERVAL</th><th>NO OF ESTABLISHMENT</th><th>NO OF BRANCHES</th><th>COST</th><th>VISIBILITY</th>";
@@ -93,6 +104,15 @@
 								$('#visibility').prop('checked', true);
 							else 
 								$('#visibility').prop('checked', false);
+
+							if ($('#planDuration option:selected').text() == "custom") {
+								$('#customPlan').show();
+								$('#customPlanDuration').val(jsonObj.custom_interval);
+								$('#intervalCount').val(jsonObj.interval_count);
+								//console.log("custom plan");
+							} else {
+								$('#customPlan').hide();
+							}							
 						}
 					}				
 				}
@@ -135,11 +155,20 @@
 
 				$(document).on('click', '.optEdit', function() {
 					//Hides uneditable fields
-					$('#interval').hide();
-					$('#estab_no').hide();
-					$('#branch_no').hide();
-					$('#cost').hide();
+					//$('#interval').hide();
+					// $('#estab_no').hide();
+					// $('#branch_no').hide();
+					// $('#cost').hide();
 					//end of hiding fields
+
+					//Disable fields
+					$('#estab_no').prop('disabled', true);
+					$('#branch_no').prop('disabled', true);
+					$('#cost').prop('disabled', true);
+					$('#planDuration').prop('disabled', true);
+					$('#customPlanDuration').prop('disabled', true);
+					$('#intervalCount').prop('disabled', true);
+					//disable fields
 
 					$('#optAdd').hide();
 					$('#optSave').show();
@@ -163,10 +192,18 @@
 					$('#visibility').prop('checked', false);	
 
 					//show the fields
-					$('#interval').show();
-					$('#estab_no').show();
-					$('#branch_no').show();
-					$('#cost').show();					
+					// $('#interval').show();
+					// $('#estab_no').show();
+					// $('#branch_no').show();
+					// $('#cost').show();	
+
+					//enable fields
+					$('#estab_no').prop('disabled', false);
+					$('#branch_no').prop('disabled', false);
+					$('#cost').prop('disabled', false);
+					$('#planDuration').prop('disabled', false);			
+					$('#customPlanDuration').prop('disabled', false);
+					$('#intervalCount').prop('disabled', false);				
 				});
 
 				$('#optSave').on('click', function(e) {
