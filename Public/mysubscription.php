1dc5535d11f315684112c89a1eb94b9af9051805
Emmanuel Yasa <codeyasam@gmail.com>
2016-10-11 05:23:33 +0800

    [PHP - mysubscription, subsplan model] fixes bug when stripe admin forces to delete a subscription plan

--- Public/mysubscription.php
+++ Public/mysubscription.php
@@ -6,6 +6,7 @@
 	$condition['value'] = $user->id;
 	$condition['isNumeric'] = true;
 	$transactions = SubsPlan::find_all($condition);	
+	$transactions = SubsPlan::deleteNonExistentPlans($transactions, $condition);
 ?>
 <?php  //Handle 
 	function stripeHasSubscription($stripe_id, $id) {
@@ -37,54 +38,59 @@
 	</header>
 	<div class="container center">
 		<h1>My Subscription</h1>
-		<table id="subsContainer" class="data">
-			<tr>
-				<th>Subscription</th>
-				<th>Status</th>
-				<th colspan="3">options</th>
-			</tr>
-			<?php foreach($transactions as $key => $each_transac):  ?>
-				
-				<?php $plan = Plan::find_by_id($each_transac->plan_id); ?>
-				<?php //$stripeSubs = \Stripe\Subscription::retrieve($each_transac->stripe_id); ?>
-				<?php $stripeSubs = stripeHasSubscription($each_transac->stripe_id, $each_transac->id); ?>
-				<?php //$stripeSubs->cancel(array('at_period_end' => true));?>
-				<?php //$stripeSubs->plan = $plan->id; $stripeSubs->save(); ?>
-				
+		<?php if (!empty($transactions)) { ?>
+			<table id="subsContainer" class="data">
 				<tr>
-					<?php $updated_transact = SubsPlan::find_by_id($each_transac->id); ?>
-					<td><?php echo $plan->toString(); ?></td>
-					<td><?php echo $updated_transact->status; ?></td>
-					<td><a href="manageEstab.php?sbscrbdID=<?php echo urlencode($each_transac->id); ?>">MANAGE ESTABLISHMENT</a></td>			
-					<input type="hidden" name="subsPlanID" value="<?php echo $each_transac->id; ?>"/>
-					<td>
-					
-					<?php 
-						if ($stripeSubs !== false) {
-							if ($stripeSubs->cancel_at_period_end === true) {
-							?><form action="cancelSubscription.php?id=<?php echo urlencode($each_transac->id); ?>&opt=REACT" method="POST">
-								<input type="submit" name="submit" value="REACTIVATE RECURRING"/>
-							<?php 
-								//echo "REACTIVATE RECURRING";
-							} else {
-							?><form action="cancelSubscription.php?id=<?php echo urlencode($each_transac->id); ?>&opt=CANCEL" method="POST">
-								<input type="submit" name="submit" value="CANCEL AT PERIOD END"/>
-							<?php
-								//echo "CANCEL AT PERIOD END";
-							}
-						} else {
-							?><form action="cancelSubscription.php?id=<?php echo urlencode($each_transac->id); ?>&opt=RENEW" method="POST">
-								<input type="submit" name="submit" value="RENEW"/>
-							<?php
-								//echo "RENEW";
-						}
-					?>
-
-					</form></td>
+					<th>Subscription</th>
+					<th>Status</th>
+					<th colspan="3">options</th>
 				</tr>
-				
-			<?php endforeach; ?>		
-		</table>
+
+				<?php foreach($transactions as $key => $each_transac):  ?>
+					
+					<?php $plan = Plan::find_by_id($each_transac->plan_id); ?>
+					<?php //$stripeSubs = \Stripe\Subscription::retrieve($each_transac->stripe_id); ?>
+					<?php $stripeSubs = stripeHasSubscription($each_transac->stripe_id, $each_transac->id); ?>
+					<?php //$stripeSubs->cancel(array('at_period_end' => true));?>
+					<?php //$stripeSubs->plan = $plan->id; $stripeSubs->save(); ?>
+					
+					<tr>
+						<?php $updated_transact = SubsPlan::find_by_id($each_transac->id); ?>
+						<td><?php echo $plan->toString(); ?></td>
+						<td><?php echo $updated_transact->status; ?></td>
+						<td><a href="manageEstab.php?sbscrbdID=<?php echo urlencode($each_transac->id); ?>">MANAGE ESTABLISHMENT</a></td>			
+						<input type="hidden" name="subsPlanID" value="<?php echo $each_transac->id; ?>"/>
+						<td>
+						
+						<?php 
+							if ($stripeSubs !== false) {
+								if ($stripeSubs->cancel_at_period_end === true) {
+								?><form action="cancelSubscription.php?id=<?php echo urlencode($each_transac->id); ?>&opt=REACT" method="POST">
+									<input type="submit" name="submit" value="REACTIVATE RECURRING"/>
+								<?php 
+									//echo "REACTIVATE RECURRING";
+								} else {
+								?><form action="cancelSubscription.php?id=<?php echo urlencode($each_transac->id); ?>&opt=CANCEL" method="POST">
+									<input type="submit" name="submit" value="CANCEL AT PERIOD END"/>
+								<?php
+									//echo "CANCEL AT PERIOD END";
+								}
+							} else {
+								?><form action="cancelSubscription.php?id=<?php echo urlencode($each_transac->id); ?>&opt=RENEW" method="POST">
+									<input type="submit" name="submit" value="RENEW"/>
+								<?php
+									//echo "RENEW";
+							}
+						?>
+
+						</form></td>
+					</tr>
+					
+				<?php endforeach; ?>		
+			</table>
+		<?php } else { ?>
+			<p>You dont have any subscriptions yet. <a href="subscription.php">Pick a subscription here.</a></p>
+		<?php } ?>		
 		<footer>
 			<?php include '../includes/footer.php'; ?>
 		</footer>
