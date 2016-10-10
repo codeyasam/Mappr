6ba7386bed4bfe97c8dc5777820a069f29b4652f
Emmanuel Yasa <codeyasam@gmail.com>
2016-10-11 04:05:43 +0800

    [PHP, STRIPE] Fixes stripe error when transaction is empty

--- Public/paymentSummary.php
+++ Public/paymentSummary.php
@@ -6,7 +6,10 @@
 	$condition['value'] = $user->id;
 	$condition['isNumeric'] = true;
 	//$transactions = SubsPlan::find_all($condition);
-	$transactions = \Stripe\Invoice::all(array("customer" => $user->stripe_id));
+	//$user->stripe_id = "cus_9Jvf01ISXoiUSO"; //debug 
+	$transactions = "";
+	if (!empty($user->stripe_id)) 
+		$transactions = \Stripe\Invoice::all(array("customer" => $user->stripe_id));
 	// echo "<pre>";
 	// 	print_r($transactions);
 	// echo "</pre>";
@@ -27,36 +30,40 @@
 	<div class="container center">
 
 		<h1>Transactions</h1>
-
-		<table class="data">
-			<tr>
-				<th>PRICE</th>
-				<th>DISCOUNT</th>
-				<th>AMOUNT PAID</th>
-				<th>DATE: (timezone - server)</th>
-			</tr>	
-		<?php for ($i = 0; $i < count($transactions['data']); $i++) { ?>
-			<?php $each_transac = $transactions['data'][$i]; ?>
-			<?php $total_amount = $each_transac->total / 100; ?>
-			<?php $subtotal = $each_transac->subtotal / 100; ?>
-			<?php  
-				$discount = "none";
-				if (!empty($each_transac->discount)) {
-					$coupon = $each_transac->discount->coupon;
-					if (!empty($coupon->percent_off)) {
-						$discount = $coupon->percent_off . "%";
-					} else {
-						$discount = $coupon->amount_off / 100 . " " . $coupon->currency;
+		<?php if (!empty($transactions)) { ?>
+			<table class="data">
+				<tr>
+					<th>PRICE</th>
+					<th>DISCOUNT</th>
+					<th>AMOUNT PAID</th>
+					<th>DATE: (timezone - server)</th>
+				</tr>	
+			<?php for ($i = 0; $i < count($transactions['data']); $i++) { ?>
+				<?php $each_transac = $transactions['data'][$i]; ?>
+				<?php $total_amount = $each_transac->total / 100; ?>
+				<?php $subtotal = $each_transac->subtotal / 100; ?>
+				<?php  
+					$discount = "none";
+					if (!empty($each_transac->discount)) {
+						$coupon = $each_transac->discount->coupon;
+						if (!empty($coupon->percent_off)) {
+							$discount = $coupon->percent_off . "%";
+						} else {
+							$discount = $coupon->amount_off / 100 . " " . $coupon->currency;
+						}
 					}
-				}
-	  		?>
-			<tr>
-				<td><?php echo htmlentities($subtotal); ?></td>
-				<td><?php echo htmlentities($discount); ?></td>
-				<td><?php echo htmlentities($total_amount); ?></td>
-				<td><?php echo htmlentities(format_date(get_mysql_datetime($each_transac->date))); ?></td>
-			</tr>
-		<?php } ?>		
+		  		?>
+				<tr>
+					<td><?php echo htmlentities($subtotal); ?></td>
+					<td><?php echo htmlentities($discount); ?></td>
+					<td><?php echo htmlentities($total_amount); ?></td>
+					<td><?php echo htmlentities(format_date(get_mysql_datetime($each_transac->date))); ?></td>
+				</tr>
+			<?php } ?>
+		<?php } else { ?>
+		<?php echo "you dont have any transactions yet. "; ?>	
+		<a href="subscription.php">purchase a subscription here</a>
+		<?php } ?>
 		</table>
 		<footer>
 			<?php include '../includes/footer.php'; ?>
