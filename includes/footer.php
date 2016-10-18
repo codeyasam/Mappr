473dee305ca382a287b08be787271f20f31f4cea
Emmanuel Yasa <codeyasam@gmail.com>
2016-10-18 17:51:49 +0800

    [JS] fixes bug on jquery version

--- includes/footer.php
+++ includes/footer.php
@@ -2,5 +2,5 @@
 		<p>&copy; Mappr <?php echo date("Y"); ?></p>
 	</footer>
 	
-<script src="js/jquery.js"></script>
+<script src="js/jquery-1.11.3.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
