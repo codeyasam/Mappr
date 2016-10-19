c0b3efb56b6ba2b67c70fdc3408a5b6bc5e18166
Emmanuel Yasa <codeyasam@gmail.com>
2016-10-20 02:10:54 +0800

    [PHP - Admin backendprocess] Fixes bug with logging activities

--- Public/Admin/backendprocess.php
+++ Public/Admin/backendprocess.php
@@ -1,4 +1,9 @@
 <?php require_once("../../includes/initialize.php"); ?>
+<?php $session->is_logged_in() ? null : redirect_to("../index.php"); ?>
+<?php  
+	$user = User::find_by_id($session->user_id);
+	$user->user_type != "SUPERADMIN" && $user->user_type != "ADMIN" ? redirect_to("../index.php") : null;	
+?>
 <?php  	
 	$output = "{";
 	//For category management
